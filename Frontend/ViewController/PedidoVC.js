8a63b555978ff168fecb93e17483b6f91d6e549b
Pablo <pabloteruel077@gmail.com>
2025-12-04 16:30:33 +0100

    terminada la pantalla de pedidos

--- Frontend/ViewController/PedidoVC.js
+++ Frontend/ViewController/PedidoVC.js
@@ -8,42 +8,44 @@ function cargarPedidos() {
     .then(r => r.json())
     .then(data => {
 
-        if (!data.exito) {
-            alert(data.mensaje);
-            return;
-        }
-
         let tbody = document.getElementById("tbodyPedidos");
         tbody.innerHTML = "";
 
+        let total = 0, frios = 0, calientes = 0;
+
         data.datos.forEach(function(p) {
 
-            let boton = "";
-            if (p.estado === "Pendiente") {
-                boton = "<button onclick='marcarEntregado(" + p.id + ")'>Entregar</button>";
-            } else {
-                boton = "<span style='color:green;font-size:20px;'>✔</span>";
-            }
+            total++;
+            if (p.tipo === "frio") frios++;
+            if (p.tipo === "caliente") calientes++;
 
-            let tr = document.createElement("tr");
+            let estadoClass = (p.estado === "Pendiente") ? "entregaPendiente" : "entregaCompleta";
 
-            tr.innerHTML = `
-                <td>${p.id}</td>
-                <td>${p.alumno}</td>
-                <td>${p.bocadillo}</td>
-                <td>${p.tipo}</td>
-                <td>${p.fecha}</td>
-                <td>${p.estado}</td>
-                <td>${boton}</td>
+            let boton = (p.estado === "Pendiente")
+            ? `<button onclick="marcarEntregado(${p.id})">Entregar</button>`
+            : `<span style='color:green;font-size:20px;'>✔</span>`;
+
+            let tr = `
+                <tr>
+                    <td>${p.alumno}</td>
+                    <td>${p.bocadillo}</td>
+                    <td class="${p.tipo}">${p.tipo}</td>
+                    <td>${p.fecha}</td>
+                    <td class="${estadoClass}">${p.estado}</td>
+                    <td>${boton}</td>
+                </tr>
             `;
 
-            tbody.appendChild(tr);
+            tbody.innerHTML += tr;
         });
+
+        document.getElementById("totalPedidos").innerText = total;
+        document.getElementById("totalFrios").innerText = frios;
+        document.getElementById("totalCalientes").innerText = calientes;
     });
 }
 
 function marcarEntregado(id) {
-
     let form = new FormData();
     form.append("accion", "marcarEntregado");
     form.append("id", id);
@@ -53,13 +55,5 @@ function marcarEntregado(id) {
         body: form
     })
     .then(r => r.json())
-    .then(data => {
-
-        if (!data.exito) {
-            alert(data.mensaje);
-            return;
-        }
-
-        cargarPedidos();
-    });
+    .then(() => cargarPedidos());
 }
